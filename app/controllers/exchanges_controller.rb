166bbee1086b491ffd9181ebe18e5348331b0ca4
Fernando Eufrásio <fernando.epm1@gmail.com>
2020-01-05 12:45:47 -0300

    Define convert action for ExchangesController

--- app/controllers/exchanges_controller.rb
+++ app/controllers/exchanges_controller.rb
@@ -1,7 +1,19 @@
 class ExchangesController < ApplicationController
+  # GET '/'
   def index
   end
 
+  # GET '/convert'
   def convert
+    # Chamada do Service para realizar a conversão
+    # converted_value =
+    #   ExchangeService.new(
+    #     params[:source_currency],
+    #     params[:target_currency],
+    #     params[:amount]
+    #   )
+
+    converted_value = 1.0
+    render json: { 'value': converted_value }
   end
 end
