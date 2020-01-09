d5b37fa49018caa5ccdb47b304a5b7de8809160d
Fernando Eufrásio <fernando.epm1@gmail.com>
2020-01-09 20:50:21 -0300

    Add crypto API info to credentials

--- spec/system/exchanges_index_system_spec.rb
+++ spec/system/exchanges_index_system_spec.rb
@@ -1,15 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe "Exchange Currency Process", :type => :system, js: true do
-  it "shows exchanged value" do
+RSpec.describe 'Exchange Currency Process', :type => :system, js: true do
+  it 'shows exchanged value' do
     visit '/'
 
-    within("#exchange_form") do
+    within('#exchange_form') do
       select('EUR', from: 'source_currency')
       select('USD', from: 'target_currency')
       fill_in 'amount', with: rand(1..9999)
     end
 
-    #expect(page).to have_content("value")
+    # Needs to not be empty
+    # wait_for_ajax
+    # expect(page).to have_field('result', with: /.+/, readonly: true)
   end
 end
